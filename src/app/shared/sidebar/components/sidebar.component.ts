c419699055d6018e66ace6f8d5c569072ae17a96
Vitya <VKotov@nota.tech>
2023-12-20 18:05:21 +0300

    add logout

--- src/app/shared/sidebar/components/sidebar.component.ts
+++ src/app/shared/sidebar/components/sidebar.component.ts
@@ -3,29 +3,37 @@ import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 import { AvatarModule } from 'primeng/avatar';
+import { MenuModule } from 'primeng/menu';
 import { SideBarService } from '../services/sidebar.services';
 import { CurrentUserI } from '../../types/currentUser.interface';
 import { UserService } from '../../services/user.service';
 import { AuthState } from '../../services/authState.state';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   standalone: true,
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
-  imports: [CommonModule, SidebarModule, ButtonModule, AvatarModule],
+  imports: [
+    CommonModule,
+    SidebarModule,
+    ButtonModule,
+    AvatarModule,
+    MenuModule,
+  ],
   providers: [SideBarService],
 })
 export class SideBarComponent implements OnInit, OnDestroy {
   sideBarVisible = false;
   currentUser!: CurrentUserI;
   subscription!: Subscription;
+  items: MenuItem[] = this.sidebarService.userItems;
   constructor(
     private userService: UserService,
-    private authState: AuthState,
-    private router: Router
+    private sidebarService: SideBarService
   ) {}
   ngOnInit(): void {
     this.initializeValues();
@@ -37,11 +45,11 @@ export class SideBarComponent implements OnInit, OnDestroy {
         this.currentUser = currentUser;
       },
       error: () => {
-        this.authState.isLoggedInState.set(false);
-        this.router.navigateByUrl('/login');
+        this.sidebarService.logout();
       },
     });
   }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
--- src/app/shared/sidebar/services/sidebar.services.ts
+++ src/app/shared/sidebar/services/sidebar.services.ts
@@ -1,6 +1,33 @@
 import { Injectable } from '@angular/core';
+import { AuthState } from '../../services/authState.state';
+import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
 
 @Injectable()
 export class SideBarService {
-  constructor() {}
+  private readonly items: MenuItem[] = [
+    {
+      label: 'Меню',
+      items: [
+        {
+          label: 'Выйти',
+          icon: 'pi pi-sign-out',
+          command: () => {
+            this.logout();
+          },
+        },
+      ],
+    },
+  ];
+
+  get userItems(): MenuItem[] {
+    return this.items;
+  }
+
+  constructor(private authState: AuthState, private router: Router) {}
+  logout(): void {
+    this.authState.isLoggedInState.set(false);
+    localStorage.clear();
+    this.router.navigateByUrl('/login');
+  }
 }
