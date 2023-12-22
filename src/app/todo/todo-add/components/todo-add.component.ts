7691fb4a808a272afea6adc3ca513e2174754795
Vitya <VKotov@nota.tech>
2023-12-22 14:32:48 +0300

    style fix

--- src/app/todo/todo-add/components/todo-add.component.ts
+++ src/app/todo/todo-add/components/todo-add.component.ts
@@ -1,14 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  SimpleChanges,
-  signal,
-} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { TodoAddService } from '../services/todo-add.service';
 import { DialogModule } from 'primeng/dialog';
@@ -16,7 +7,6 @@ import { InputTextModule } from 'primeng/inputtext';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
-import { CategoriesI } from '../../todo-list/types/categories.interface';
 @Component({
   standalone: true,
   selector: 'app-todo-add',
@@ -38,10 +28,10 @@ export class TodoAddComponent implements OnInit {
   @Input() category!: number;
   form!: FormGroup;
   priorities: object[] = [
-    { title: 'Priority 1', priority: 1 },
-    { title: 'Priority 2', priority: 2 },
-    { title: 'Priority 3', priority: 3 },
-    { title: 'Priority 4', priority: 4 },
+    { title: 'Приоритет 1', priority: 1 },
+    { title: 'Приоритет 2', priority: 2 },
+    { title: 'Приоритет 3', priority: 3 },
+    { title: 'Приоритет 4', priority: 4 },
   ];
   constructor(
     private fb: FormBuilder,
@@ -52,7 +42,6 @@ export class TodoAddComponent implements OnInit {
     console.log(this.category);
   }
   initializeForm() {
-    let a = [...this.priorities];
     this.form = this.fb.group({
       description: '',
       date: '',
