e22fa500c2f3a05ae3a26f0a2d8572acb5477286
Vitya <VKotov@nota.tech>
2024-01-24 12:51:09 +0300

    fix setAll category

--- src/app/shared/components/category/services/category.service.ts
+++ src/app/shared/components/category/services/category.service.ts
@@ -1,16 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { Observable, map } from 'rxjs';
+import { Observable, forkJoin, map } from 'rxjs';
 
 import { AppState } from 'src/app/shared/services/appState.state';
 import { environment } from 'src/environments/environment';
 import { AddCategoryI, CategoryI } from '../types/category.interface';
+import { TaskI } from 'src/app/todo/todo-list/types/task.interface';
 
 @Injectable()
 export class CategoryService {
   constructor(private http: HttpClient, private appState: AppState) {}
   addCategory(category: AddCategoryI): Observable<void> {
+    if (category.setAll === true) {
+      const req = [
+        this.http.post<CategoryI[]>(environment.apiUrl + 'category', category),
+        this.http.get(environment.apiUrl + 'tasks'),
+      ];
+      return forkJoin(req).pipe(
+        map((response) => {
+          this.appState.categories.set(response[0] as CategoryI[]),
+            this.appState.task.set(response[1] as TaskI[]);
+        })
+      );
+    }
+
     return this.http
       .post<CategoryI[]>(environment.apiUrl + 'category', category)
       .pipe(
