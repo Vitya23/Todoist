07ba8db3a15a4cf8afe1f3f64c5d9fde10c09e09
Vitya <VKotov@nota.tech>
2024-01-24 14:00:57 +0300

    remove imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserService } from './shared/services/user.service';
-import { CommonModule } from '@angular/common';
 import { AppState } from './shared/services/appState.state';
 import { HeaderComponent } from './shared/components/header/components/header.component';
 import { MessagesModule } from 'primeng/messages';
--- src/app/shared/components/delete/components/delete.component.ts
+++ src/app/shared/components/delete/components/delete.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { DeleteService } from '../services/delete.service';
 import { ButtonModule } from 'primeng/button';
