5f99f2034e718cd599ec631419359ce405787d02
Vitya <VKotov@nota.tech>
2024-02-02 19:02:00 +0300

    add ru locale to the calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { AppState } from './shared/services/appState.state';
 import { HeaderComponent } from './shared/components/header/components/header.component';
 import { MessagesModule } from 'primeng/messages';
+import { PrimeNGConfig } from 'primeng/api';
+import { ruLocale } from './locale';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,4 +15,10 @@ import { MessagesModule } from 'primeng/messages';
 export class AppComponent {
   title = 'Todoist';
   isLoggedIn = inject(AppState).isLoggedInState;
+
+  constructor(private config: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.config.setTranslation(ruLocale);
+  }
 }
